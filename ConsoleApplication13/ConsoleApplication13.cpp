5523e7f05eaff2a420559b5a3cefa55da02f670f
Norako_ <worlhentai69@outlook.com>
2021-10-16 14:56:33 +0300

    добуток это больно

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#define _USE_MATH_DEFINES 
+#include "math.h"
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double  x, y;
+    int k,s;
+    cout << "Уведіть x ";
+    cin >> x;
+    for (s=0, k = 1; k < 11; k++)
+    {
+        s = s + k;
+    }
+    y = (sin(s * M_PI / 180.)+pow(x, 2.))/(pow(x,2.)+2);
+    cout << "Y = " << y << endl;
+
+}
